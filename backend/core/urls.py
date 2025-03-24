4e06e1877ff74002e2bd06e0c22cffba04d71397
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-24 23:11:50 +0100

    chore: remove api docs view since it is not working

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path('api/', include('properties.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
